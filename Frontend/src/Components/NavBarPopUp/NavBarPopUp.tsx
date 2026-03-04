0eea5e1f7678e43cafcb6b5d1bdb8404788fc1ad
Kabilesh-GS <kabileshgs@gmail.com>
2026-03-04 23:38:17 +0530

    completed sidebar structure

--- Frontend/src/Components/NavBarPopUp/NavBarPopUp.tsx
+++ Frontend/src/Components/NavBarPopUp/NavBarPopUp.tsx
@@ -1,3 +1,5 @@
+import logo from '../../../public/Logo transparent.png'
+
 type SideBar = {
   closeSideBar : any
 }
@@ -5,7 +7,10 @@ type SideBar = {
 export default function NavBarPopUp({closeSideBar} : SideBar) {
   return (
     <div className="bg-gray-100 w-[300px] h-full absolute z-20">
-      <button onClick={closeSideBar}>X</button>
+      <div className='flex justify-between items-center w-full px-3 py-3'>
+        <img src={logo} className='w-[50px]'/>
+        <p  onClick={closeSideBar} className="bg-white py-1 px-3 cursor-pointer rounded-full">X</p>
+      </div>
     </div>
   )
 }
